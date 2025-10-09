c1134121441d34e6777c265b1001b74223587e6c
ANURAG SHARAN (RA2411003012014) <as9771@srmist.edu.in>
2025-10-09 03:26:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2992095150-452ba",
+  "appId": "1:513729394445:web:e68adba76432ea803d6244",
+  "apiKey": "AIzaSyDqgpvGBA2jkyUL9A-wOXwg1PyZ0Kg7-V4",
+  "authDomain": "studio-2992095150-452ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "513729394445"
+};
